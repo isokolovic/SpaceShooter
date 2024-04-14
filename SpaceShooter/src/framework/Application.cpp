0de719dac358226437f3e8d3bc5f2ac8d81c4302
Ivan Sokolović <isokolovic1@gmail.com>
2024-04-14 09:25:39 +0200

    Added functions for handling frame rate

--- SpaceShooter/src/framework/Application.cpp
+++ SpaceShooter/src/framework/Application.cpp
@@ -1,14 +1,20 @@
 #include "framework/Application.h"
+#include <iostream>
 
 namespace ly{
 	Application::Application()
-		: mWindow{ sf::VideoMode(600, 800), "Space Shooter" }
+		: mWindow{ sf::VideoMode(600, 800), "Space Shooter" }, 
+		mTargetFrameRate{60.f},
+		mTickClock{}
 	{
-
 
 	}
 	void Application::Run()
 	{
+		mTickClock.restart();
+		float accumulatedTime = 0.f;
+		float targetDeltaTime = 1.f / mTargetFrameRate;
+
 		//If Event is close, close the window
 		while (mWindow.isOpen()) {
 			sf::Event windowEvent;
@@ -17,6 +23,20 @@ namespace ly{
 					mWindow.close();
 				}
 			}
+
+			accumulatedTime += mTickClock.restart().asSeconds();
+			while (accumulatedTime > targetDeltaTime)
+			{
+				accumulatedTime -= targetDeltaTime;
+				Tick(targetDeltaTime);
+			}
 		}
+	}
+	void Application::Tick(float deltaTime)
+	{
+		std::cout << "Ticking at frame rate: " << 1.f / deltaTime << std::endl;
+	}
+	void Application::Render()
+	{
 	}
 }
