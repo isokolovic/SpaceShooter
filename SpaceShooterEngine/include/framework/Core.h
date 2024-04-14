9c21a2f9d8b33f611faaa4874cc5811bc09d5415
Ivan Sokolović <isokolovic1@gmail.com>
2024-04-14 14:34:52 +0200

    Defined custom types and pointers

--- SpaceShooterEngine/include/framework/Core.h
+++ SpaceShooterEngine/include/framework/Core.h
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <memory>
+#include <map>
+#include <unordered_map>
 
 namespace ly {
+
+//Define smart pointers:
+template<typename T>
+using unique = std::unique_ptr<T>;
+
+template<typename T>
+using shared = std::shared_ptr<T>;
+
+template<typename T>
+using weak = std::weak_ptr<T>;
+
+//Define structures:
+template<typename T>
+using List = std::vector<T>;
+
+template<typename keyType, typename valType, typename Pr = std::less <keyType>> //Pr: Least keyType value will be on front
+using Map = std::map<keyType, valType, Pr>;
+
+template<typename keyType, typename valType, typename Hasher = std::hash <keyType>>
+using Dictionary = std::unordered_map<keyType, valType, Hasher>;
+
 
 //Macro for logging the frame rate
 #define LOG(M, ...) printf(M "\n", ##__VA_ARGS__)
