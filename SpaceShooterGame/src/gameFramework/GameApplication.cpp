ceb8c656b3bb568c44d5163afcf51a7ab016a1ba
Ivan Sokolović <isokolovic1@gmail.com>
2024-04-17 22:08:25 +0200

    Added a Spaceship class. Added a movement logic to the Spaceship

--- SpaceShooterGame/src/gameFramework/GameApplication.cpp
+++ SpaceShooterGame/src/gameFramework/GameApplication.cpp
@@ -2,6 +2,7 @@
 #include <framework/World.h>
 #include "framework/Actor.h"
 #include "config.h"
+#include "spaceship/Spaceship.h"
 
 ss::Application* GetApplication() {
 
@@ -15,17 +16,19 @@ namespace ss {
 		weak<World> newWorld = LoadWorld<World>();
 
 		newWorld.lock()->SpawnActor<Actor>(); //Because it's a weak reference, must be locked first to get a pointer. 
-		actorToDestroy = newWorld.lock()->SpawnActor<Actor>();
-		actorToDestroy.lock()->SetTexture(GetResourceDir() + "/SpaceShooterRedux/PNG/playerShip1_blue.png");
+		testPlayerSpaceship = newWorld.lock()->SpawnActor<Spaceship>();
+		testPlayerSpaceship.lock()->SetTexture(GetResourceDir() + "/SpaceShooterRedux/PNG/playerShip1_blue.png");
 
-		actorToDestroy.lock()->SetActorLocation(sf::Vector2f(300.f, 400.f));
-		actorToDestroy.lock()->SetActorRotation(180.f);
+		testPlayerSpaceship.lock()->SetActorLocation(sf::Vector2f(300.f, 400.f));
+		testPlayerSpaceship.lock()->SetActorRotation(0.f);
+
+		testPlayerSpaceship.lock()->SetVelocity(sf::Vector2f(0.f, -200.f));
 
 		counter = 0;
 	}
 
 	void GameApplication::Tick(float deltaTime)
-	{
+	{	
 		//counter += deltaTime;
 		//if (counter > 2.f) { //Test: after 2 sec. destroy an actor
 		//	if (!actorToDestroy.expired()) {
